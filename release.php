68e10ccba3ed936f4987f658cd1b91190f78aea6
root005 <1726690348@qq.com>
2017-08-29 17:16:55 +0800

    修改：     App/Admin/View/Config/finance.html 	修改：     release.php

---
+++ release.php
@@ -0,0 +1,17 @@
+<?php  
+$con = mysql_connect("127.0.0.1","root","special005");
+
+if($con){
+    mysql_select_db("ec_deal", $con);
+    //查询积分释放比例
+    $sql='SELECT value FROM yang_config where `key`="release_rate"';
+    $release_rate = mysql_query($sql);
+    $row = mysql_fetch_array($release_rate);
+    print_r($row);
+}else{
+    die;
+}
+
+mysql_close($con);
+
+?>
