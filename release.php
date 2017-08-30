73da4fe405cdc4d4512469f315c447c603f5bb13
root005 <1726690348@qq.com>
2017-08-30 15:17:37 +0800

    修改：     App/Admin/Controller/IndexController.class.php 	修改：     App/Admin/View/Config/finance.html 	修改：     App/Admin/View/Config/information.html 	修改：     App/Home/View/Login/findPwd.html 	修改：     App/Home/View/Login/index.html 	修改：     App/Home/View/Public/header.html 	修改：     release.php 	新文件：   release.sh

--- release.php
+++ release.php
@@ -6,13 +6,24 @@ if($con){
     //查询积分释放比例
     $sql='SELECT value FROM yang_config where `key`="release_rate"';
     $release_rate = mysql_query($sql);
-    $rate = mysql_fetch_array($release_rate)[0];
-    
-    print_r($row);
+    $rate = (float)mysql_fetch_array($release_rate)[0]/100;
+    //查询yang_score_user并释放积分
+    $sql='SELECT * FROM yang_score_user where end_score <> 0';
+    $result=mysql_query($sql);
+
+    while($row = mysql_fetch_array($result)){
+        $del_sc=$row['source_score']*$rate;
+        #修改剩余平台积分
+        $sql='UPDATE yang_score_user set end_score=if(end_score-'.$del_sc.'>=0,end_score-'.$del_sc.',0) where id='.$row['id'];
+        mysql_query($sql);
+        
+        #释放后的积分
+        $sql='UPDATE yang_currency_user set num=num+'.$del_sc.' where currency_id=1 and member_id='.$row['member_id'];
+        mysql_query($sql);
+    }
 }else{
     die;
 }
-
 mysql_close($con);
 
 ?>
